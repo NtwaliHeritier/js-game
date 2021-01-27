296d9d876943d0f57818a07d02898d760f5ba808
NtwaliHeritier <heritierntwali@gmail.com>
2021-01-27 13:35:01 +0200

    fix linter errors

--- src/Objects/ApiHandling.js
+++ src/Objects/ApiHandling.js
@@ -13,16 +13,14 @@ const ApiHandling = (() => {
         headers: { 'Content-type': 'application/json; charset=UTF-8' },
       },
     )
-      .then((response) => {
-        return  response.json();
-      })
+      .then((response) => response.json())
       .then((json) => json)
       .catch((err) => err);
   };
 
   const showScores = async () => {
     const data = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/moPgZTWHxTnA8yxsQQ79/scores/');
-    return data.json();;
+    return data.json();
   };
 
   return { addScore, showScores };
